65633def9f7c32ad2b1765d2eeffc2263a6c4374
Erick Vieira <erick.vieira@outlook.com.br>
2021-03-25 22:51:33 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -19,12 +20,87 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
+
+	User::verifyLogin();
     
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
 });
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+});
+
+$app->get("/admin/users", function() {
+
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users");
+
+});
+
+$app->get("/admin/users/create", function() {
+
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+	
+});
+
+$app->get("/admin/users/:iduser", function($iduser) {
+
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update");
+	
+});
+
+$app->post("/admin/users/create", function(){
+
+
+
+});
+
+$app->post("/admin/users/:iduser", function($iduser){
+
+	User::verifyLogin();
+	
+});
+
+$app->delete("/admin/users/:iduser", function($iduser){
+
+	User::verifyLogin();
+	
+});
+
 
 $app->run();
 
